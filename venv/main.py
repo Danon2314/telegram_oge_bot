bd86bf5bd9265c32cd439dbbd03d20baa0937911
Danon2314 <9094583259d@gmail.com>
2025-12-30 00:09:37 +0300

    Добавление основных файлов для работы и базовой структуры main.py

---
+++ venv/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Включаем логирование
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота
+bot = Bot(token="8330013656:AAGZMNIYWpF_6Tfv13ZlgaflNNQvhW-WnmE")
+# Диспетчер
+dp = Dispatcher()
+
+
+# Запуск поллинга
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
